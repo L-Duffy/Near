3a313421366b40c6071282314152b67971f27e98
Lucas Duffy <lucas.a.duffy@gmail.com>
2022-09-30 14:17:30 -0400

    followed tutorial parts 1-3. rooms and DMs fully functional

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'rooms/index'
+  get 'users/show'
+  resources :rooms do
+    resources :messages
+  end
   root "pages#home"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -7,7 +10,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get "users", to: "devise/sessions#new"
   end
-
+  get "user/:id", to: "users#show", as: "user"
   # Defines the root path route ("/")
   # root "articles#index"
 end
