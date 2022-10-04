68021ba55797fea83f0ff6911619cc4feabe7d9d
Lucas Duffy <lucas.a.duffy@gmail.com>
2022-10-04 16:22:52 -0400

    started on avatars and user status, but ran in to a bug with devise which i currently cannot resolve. will revisit soon.

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,7 @@ class Message < ApplicationRecord
   belongs_to :room
   after_create_commit { broadcast_append_to room }
   before_create :confirm_participant
+  has_many_attached :attachments, dependent: :destroy
 
   def confirm_participant
     return unless room.is_private
